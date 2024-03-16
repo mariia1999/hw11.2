1a1af4accb487835b4fd486f09aa5e040956a9d8
Maria Lomets <marusya.lomets@gmail.com>
2024-03-16 10:36:38 +0300

    add func add discription

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def upper_str(string):
+    """делает все буквы заглавными"""
     return string.upper()
+
+
+def upper_letter(string):
+    """делает первые буквы заглавными"""
+    return string.title()
+
